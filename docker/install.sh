bee369466bbd35d4a9cea3c8fe084faea7f0ef20
Maurizio Pillitu <maurizio@session.it>
2014-08-26 09:45:16 +0200

    testing and fixing docker build for Alfresco Enterprise clustered environment
    Former-commit-id: 9bb25d42576356282af3c2fe3d4ee0eb4d241ec4 [formerly 9bb25d42576356282af3c2fe3d4ee0eb4d241ec4 [formerly 9bb25d42576356282af3c2fe3d4ee0eb4d241ec4 [formerly 34290a9f72eacd5eb7c8c5c1a78f5f46278be988]]]
    Former-commit-id: 04eb131244c586a27635429e167a177b9c939c53
    Former-commit-id: 53202a6e9bd990a77781c320dc09a74e61dd4c32
    Former-commit-id: 67cef1804b669b15358defbd4ddb6452d85d50e5
    Former-commit-id: 56d712affe9cbe507a5e1f398ddb277abac7a2d0

--- docker/install.sh
+++ docker/install.sh
@@ -1,25 +1,15 @@
-#To install latest Docker version
-sudo bash -c 'echo export PATH=/opt/packer:$PATH > /etc/profile.d/packer-path.sh'
-sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
-sudo sh -c "touch /etc/apt/sources.list.d/docker.list; echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-
-#Installing docker, unzip and packer
-sudo apt-get  -qqy update
-sudo apt-get  -qqy install lxc-docker
-sudo apt-get  -qqy install mysql-client
-sudo service docker restart
-sudo update-rc.d docker defaults
-sudo chmod 777 /var/run/docker.sock
-
 #Installing Packer
-sudo apt-get  -qqy install unzip
-wget https://dl.bintray.com/mitchellh/packer/0.6.0_linux_amd64.zip
-sudo mkdir -p /opt/packer; sudo unzip 0.6.0_linux_amd64.zip -d /opt/packer
-
-#Add packer bins in PATH
-sudo touch /etc/profile.d/packer-path.sh
+wget https://dl.bintray.com/mitchellh/packer/0.6.1_linux_amd64.zip
+sudo mkdir -p /opt/packer; sudo unzip 0.6.1_linux_amd64.zip -d /opt/packer
 
 #Download Docker images
 docker pull tutum/mysql:latest
 docker pull maoo/alf-precise-base
-docker pull busybox
+# @TODO - may be used for data container, but not working yet
+#docker pull busybox
+
+# @TODO This part is still not working, needs to be executed manually, as documented in the README.md
+
+#cd /alfboxes/docker
+#/opt/packer/packer build precise-alf422.json
+#docker import - maoo/alf-precise:latest < precise-alf422.tar
