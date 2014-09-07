aff7fe6ba0be7a55ac7e77d8411074977b5dd4ae
Maurizio Pillitu <maurizio@session.it>
2014-09-07 03:33:34 +0200

    improved file naming

--- docker/scripts/build-base-images.sh
+++ docker/scripts/build-base-images.sh
@@ -1,18 +1,15 @@
 #Building/Importing/Publishing `alfresco-web-base` Alfresco Docker Image
 cd /alfboxes/docker/images/base/alfresco-base
 docker build -t maoo/alfresco-base:latest .
-docker push maoo/alfresco-base:latest
 
 #Building/Importing/Publishing `alfresco-web-base` Alfresco Docker Image
 cd /alfboxes/docker/images/base/alfresco-web-base
 /opt/packer/packer build alfresco-web-base.json
 docker import - maoo/alfresco-web-base:latest < alfresco-web-base.tar
 rm *.tar
-docker push maoo/alfresco-web-base:latest
 
 #Building/Importing/Publishing `alfresco-repo-base` Docker Image
 cd /alfboxes/docker/images/base/alfresco-repo-base
 /opt/packer/packer build alfresco-repo-base.json
 docker import - maoo/alfresco-repo-base:latest < alfresco-repo-base.tar
 rm *.tar
-docker push maoo/alfresco-repo-base:latest
---
+++ docker/scripts/publish-images.sh
@@ -0,0 +1,11 @@
+docker push maoo/alfresco-base:latest
+docker push maoo/alfresco-web-base:latest
+docker push maoo/alfresco-repo-base:latest
+
+docker push maoo/apache-lb:latest
+docker push maoo/alfresco-repo:latest
+docker push maoo/alfresco-share:latest
+docker push maoo/alfresco-solr:latest
+
+docker push maoo/alfresco-allinone-community:latest
+docker push maoo/alfresco-allinone-enterprise:latest
