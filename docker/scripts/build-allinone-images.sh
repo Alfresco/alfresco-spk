c7b6c19bd61b2a6135cc568769da90c4c200433a
Maurizio Pillitu <maurizio@session.it>
2014-09-07 03:33:34 +0200

    improved file naming
    Former-commit-id: e59b309435a58e5cb68a4a309c01c08221eee710 [formerly e59b309435a58e5cb68a4a309c01c08221eee710 [formerly e59b309435a58e5cb68a4a309c01c08221eee710 [formerly aff7fe6ba0be7a55ac7e77d8411074977b5dd4ae]]]
    Former-commit-id: 869b12fae3d5c80cad1d142565997bed0a9409cd
    Former-commit-id: 5226752190f90192259bdc9423643b3dbfe5a5d7
    Former-commit-id: 3562e3659eb5ef6522c6bce004e12bd637251976
    Former-commit-id: dc6673672fc9214857a4fb3a6f7153425e6c4462

--- docker/scripts/build-allinone-images.sh
+++ docker/scripts/build-allinone-images.sh
@@ -3,14 +3,12 @@ cd /alfboxes/docker/images/allinone/alfresco-allinone-community
 /opt/packer/packer build alfresco-allinone-community.json
 docker import - maoo/alfresco-allinone-community:latest < alfresco-allinone-community.tar
 rm *.tar
-docker push maoo/alfresco-allinone-community:latest
 
 # Building/Importing `alfresco-allinone` Enterprise Docker Image
 cd /alfboxes/docker/images/allinone/alfresco-allinone-enterprise
 /opt/packer/packer build alfresco-allinone-enterprise.json
 docker import - maoo/alfresco-allinone-enterprise:latest < alfresco-allinone-enterprise.tar
 rm *.tar
-docker push maoo/alfresco-allinone-enterprise:latest
 
 #Removing temporarily created containers
 docker rm `docker ps --no-trunc -a -q`
