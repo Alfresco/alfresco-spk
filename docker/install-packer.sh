a103b69a29390e2a532e37ff3df593aecb038e76
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-12-11 00:30:10 +0100

    added Vagrantfile and scripts to build docker images

---
+++ docker/install-packer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#Installing Packer
+wget https://dl.bintray.com/mitchellh/packer/0.6.1_linux_amd64.zip
+sudo mkdir -p /opt/packer; sudo unzip 0.6.1_linux_amd64.zip -d /opt/packer
---
+++ docker/pull-docker-images.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Download pre-defined Docker Images
+docker pull centos
+docker pull orchardup/mysql:latest
+
+# Not used ATM
+# docker pull crosbymichael/skydns:latest
+# docker pull crosbymichael/skydock:latest
+# docker pull dockerfile/haproxy:latest
