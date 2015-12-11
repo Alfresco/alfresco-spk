9664642ea11a131ba460e357f09f863aa9fc6131
toniblyx <toni@blyx.com>
2015-12-11 11:12:30 -0500

    added echos and fixed vagrant install

--- docker/install-vagrant.sh
+++ docker/install-vagrant.sh
@@ -2,5 +2,6 @@
 
 if [ ! -f /usr/bin/vagrant ]; then
   echo "Installing Vagrant..."
-  rpm -ihv https://releases.hashicorp.com/vagrant/1.7.4/vagrant_1.7.4_x86_64.rpm
+  wget -q https://releases.hashicorp.com/vagrant/1.7.4/vagrant_1.7.4_x86_64.rpm
+  rpm -ihv vagrant_1.7.4_x86_64.rpm
 fi
