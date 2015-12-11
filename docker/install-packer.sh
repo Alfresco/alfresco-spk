2c562049298d7c8fd3e15431f1722e998ec4d898
toniblyx <toni@blyx.com>
2015-12-11 10:49:30 -0500

    fixed packer and vagrant install

--- docker/install-packer.sh
+++ docker/install-packer.sh
@@ -4,4 +4,4 @@ yum -y install unzip
 
 #Installing Packer
 wget -q https://dl.bintray.com/mitchellh/packer/packer_0.8.6_linux_amd64.zip
-mkdir -p /opt/packer; unzip packer_0.8.6_linux_amd64.zip -d /opt/packer
+mkdir -p /opt/packer; unzip -o packer_0.8.6_linux_amd64.zip -d /opt/packer
--- docker/install-vagrant.sh
+++ docker/install-vagrant.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rph -ihv https://releases.hashicorp.com/vagrant/1.7.4/vagrant_1.7.4_x86_64.rpm
+rpm -ihv https://releases.hashicorp.com/vagrant/1.7.4/vagrant_1.7.4_x86_64.rpm
