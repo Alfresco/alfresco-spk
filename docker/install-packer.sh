bb27737fb2e9c128c08cd1822433504f7a520d3a
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-12-11 16:02:16 +0100

    fixed packer zip url

--- docker/install-packer.sh
+++ docker/install-packer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #Installing Packer
-wget https://dl.bintray.com/mitchellh/packer/0.8.6_linux_amd64.zip
+wget https://dl.bintray.com/mitchellh/packer/packer_0.8.6_linux_amd64.zip
 sudo mkdir -p /opt/packer; sudo unzip 0.8.6_linux_amd64.zip -d /opt/packer
