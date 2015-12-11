11608bbcd195401178b801691db21572aad86922
Maurizio Pillitu <maoo@apache.org>
2015-12-11 15:37:17 +0100

    Update install-packer.sh
    updated packer version

--- docker/install-packer.sh
+++ docker/install-packer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #Installing Packer
-wget https://dl.bintray.com/mitchellh/packer/0.6.1_linux_amd64.zip
-sudo mkdir -p /opt/packer; sudo unzip 0.6.1_linux_amd64.zip -d /opt/packer
+wget https://dl.bintray.com/mitchellh/packer/0.8.6_linux_amd64.zip
+sudo mkdir -p /opt/packer; sudo unzip 0.8.6_linux_amd64.zip -d /opt/packer
