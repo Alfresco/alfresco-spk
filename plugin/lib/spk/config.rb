85f8ad3fa9759745641bbf5d613f85552b725d93
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-12-29 12:48:49 +0100

    updated default values

--- plugin/lib/spk/config.rb
+++ plugin/lib/spk/config.rb
@@ -23,9 +23,9 @@ module VagrantPlugins
 				@packer_bin = 'packer' if @packer_bin == UNSET_VALUE
 				@packer_opts = '' if @packer_opts == UNSET_VALUE
 				@mode = '' if @mode == UNSET_VALUE
-				@box_url = 'http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_centos-7.1_chef-provisionerless.box' if @box_url == UNSET_VALUE
-				@box_name = "opscode_centos-7.1" if @box_name == UNSET_VALUE
-				@cookbooks_url = "https://artifacts.alfresco.com/nexus/service/local/repositories/releases/content/org/alfresco/devops/chef-alfresco/0.6.13/chef-alfresco-0.6.13.tar.gz" if @cookbooks_url == UNSET_VALUE
+				@box_url = 'http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_centos-7.2_chef-provisionerless.box' if @box_url == UNSET_VALUE
+				@box_name = "opscode_centos-7.2" if @box_name == UNSET_VALUE
+				@cookbooks_url = "https://artifacts.alfresco.com/nexus/service/local/repositories/releases/content/org/alfresco/devops/chef-alfresco/0.6.15/chef-alfresco-0.6.15.tar.gz" if @cookbooks_url == UNSET_VALUE
 				@databags_url = '' if @databags_url == UNSET_VALUE
 				@stack_template = "file://#{File.expand_path File.dirname(__FILE__)}/../../files/stack-templates/community-allinone.json" if @stack_template == UNSET_VALUE
 				@ks_template = "file://#{File.expand_path File.dirname(__FILE__)}/../../files/ks/ks-centos.cfg" if @ks_template == UNSET_VALUE
