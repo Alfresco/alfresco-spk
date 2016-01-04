d5ec97d3a5d346c07592dfbb3c121a82bf2f74d3
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2016-01-04 17:36:42 +0100

    fixed default values

--- plugin/lib/spk/commons/engine.rb
+++ plugin/lib/spk/commons/engine.rb
@@ -112,7 +112,7 @@ module VagrantPlugins
 					    merged_attrs = JSON.parse(JSON.merge(File.read("#{work_dir}/attributes-#{node_name}.json.original"), local_json_vars))
 					  end
 
-					  # merged_attrs = get_nexus_creds(merged_attrs)
+					  merged_attrs = get_nexus_creds(merged_attrs)
 
 					  attr_file = File.open("#{work_dir}/attributes-#{node_name}.json", 'w')
 					  attr_file.write(merged_attrs.to_json)
@@ -127,7 +127,7 @@ module VagrantPlugins
 				  node_url = "#{work_dir}/attributes-#{chef_node_name}.json"
 				  node_url_content = File.read("#{work_dir}/attributes-#{chef_node_name}.json.original")
 				  json_provisioner['json'] = JSON.parse(node_url_content)
-				  # json_provisioner['json'] = get_nexus_creds(json_provisioner['json'])
+				  json_provisioner['json'] = get_nexus_creds(json_provisioner['json'])
 				  return json_provisioner.to_json
 				end
 
@@ -154,20 +154,26 @@ module VagrantPlugins
 				  return ret
 				end
 
-				#TODO - this should go into instance templates
-				# def get_nexus_creds(json_attrs)
-				#   ret = json_attrs
-				#   if ENV['NEXUS_USERNAME'] and ENV['NEXUS_PASSWORD']
-				#     ret['artifact-deployer'] = {}
-				#     ret['artifact-deployer']['maven'] = {}
-				#     ret['artifact-deployer']['maven']['repositories'] = {}
-				#     ret['artifact-deployer']['maven']['repositories']['private'] = {}
-				#     ret['artifact-deployer']['maven']['repositories']['private']['url'] = "https://artifacts.alfresco.com/nexus/content/groups/private"
-				#     ret['artifact-deployer']['maven']['repositories']['private']['username'] = ENV['NEXUS_USERNAME']
-				#     ret['artifact-deployer']['maven']['repositories']['private']['password'] = ENV['NEXUS_PASSWORD']
-				#   end
-				#   return ret
-				# end
+				#TODO - this should not be here, but cannot handled within
+				# SPK provisioner, since no Packer variables are supported
+				# there
+				def get_nexus_creds(json_attrs)
+					name = ENV['MVN_CHEF_REPO_NAME']
+					url = ENV['MVN_CHEF_REPO_URL']
+					username = ENV['MVN_CHEF_REPO_USERNAME']
+					password = ENV['MVN_CHEF_REPO_PASSWORD']
+					ret = json_attrs
+				  if name and url and username and password
+				    ret['artifact-deployer'] = {}
+				    ret['artifact-deployer']['maven'] = {}
+				    ret['artifact-deployer']['maven']['repositories'] = {}
+				    ret['artifact-deployer']['maven']['repositories'][name] = {}
+				    ret['artifact-deployer']['maven']['repositories'][name]['url'] = url
+				    ret['artifact-deployer']['maven']['repositories'][name]['username'] = username
+				    ret['artifact-deployer']['maven']['repositories'][name]['password'] = password
+				  end
+				  return ret
+				end
 
 				def get_artifact(work_dir, command, url, artifact_name)
 				  # Download and uncompress Chef artifacts (in a Berkshelf package format)
--- plugin/lib/spk/config.rb
+++ plugin/lib/spk/config.rb
@@ -28,13 +28,13 @@ module VagrantPlugins
 				@mode = '' if @mode == UNSET_VALUE
 				@box_url = 'http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_centos-7.2_chef-provisionerless.box' if @box_url == UNSET_VALUE
 				@box_name = "opscode_centos-7.2" if @box_name == UNSET_VALUE
-				@cookbooks_url = "https://artifacts.alfresco.com/nexus/service/local/repositories/releases/content/org/alfresco/devops/chef-alfresco/0.6.15/chef-alfresco-0.6.15.tar.gz" if @cookbooks_url == UNSET_VALUE
+				@cookbooks_url = "file://$PWD/berks-cookbooks.tar.gz" if @cookbooks_url == UNSET_VALUE
 				@databags_url = '' if @databags_url == UNSET_VALUE
 				@stack_template = "file://#{File.expand_path File.dirname(__FILE__)}/../../files/stack-templates/community-allinone.json" if @stack_template == UNSET_VALUE
 				@pre_commands = "file://#{File.expand_path File.dirname(__FILE__)}/../../files/pre-commands.json" if @pre_commands == UNSET_VALUE
 				@post_commands = "file://#{File.expand_path File.dirname(__FILE__)}/../../files/post-commands.json" if @post_commands == UNSET_VALUE
 
-				@ks_template = "file://#{File.expand_path File.dirname(__FILE__)}/../../files/ks/ks-centos.cfg" if @ks_template == UNSET_VALUE
+				@ks_template = "https://raw.githubusercontent.com/Alfresco/alfresco-spk/master/ks/ks-centos.cfg" if @ks_template == UNSET_VALUE
 			end
 		end
 	end
