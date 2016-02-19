339bae7a911c643710233cca71d77a8274a6ecbc
Maurizio Pillitu <maoo@apache.org>
2016-02-19 23:23:50 +0100

    Update s3-upload.sh

--- scripts/chef/s3-upload.sh
+++ scripts/chef/s3-upload.sh
@@ -3,6 +3,12 @@
 # Uploads a file to an S3 location
 # It also ensures that aws commandline is installed
 
+# Example:
+# 
+# BOX_LOCATION="~/.vagrant.d/data/packer-build/*.box"
+# S3_FOLDER_URL="https://s3.amazon.com/..."
+# curl -L https://raw.githubusercontent.com/Alfresco/alfresco-spk/master/scripts/chef/s3-upload.sh --no-sessionid | bash -s -- $BOX_LOCATION $S3_FOLDER_URL
+
 if [ "$#" -ne 2 ]; then
     echo "Usage: s3-upload.sh <LocalPath> <S3Uri>"
     exit 1
