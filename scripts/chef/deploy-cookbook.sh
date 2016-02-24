79487af4f72f51f07e1ea920fc925719a7fd0f0b
Maurizio Pillitu <maoo@apache.org>
2016-02-24 10:45:55 +0000

    Update deploy-cookbook.sh
    using local function to run tests

--- scripts/chef/deploy-cookbook.sh
+++ scripts/chef/deploy-cookbook.sh
@@ -18,13 +18,13 @@ export PATH=/usr/local/packer:$HOME/.chefdk/gem/ruby/2.1.0/bin:/opt/chefdk/bin:/
 # Right now it only supports HTTP Git urls
 if [ -z "$ARTIFACT_ID" ]; then
   export ARTIFACT_ID=`echo ${GIT_REPO%????} | cut -d "/" -f 5`
-  echo "[run-release.sh] Setting ARTIFACT_ID=$ARTIFACT_ID"
+  echo "[deploy-cookbook.sh] Setting ARTIFACT_ID=$ARTIFACT_ID"
 else
-  echo "[run-release.sh] ARTIFACT_ID=$ARTIFACT_ID"
+  echo "[deploy-cookbook.sh] ARTIFACT_ID=$ARTIFACT_ID"
 fi
 
 function runTests () {
-  echo "[run-release.sh] Running Chef, Foodcritic and ERB syntax check tests"
+  echo "[deploy-cookbook.sh] Running Chef, Foodcritic and ERB syntax check tests"
   if grep -L foodcritic gems.list; then
     gem install foodcritic
   fi
@@ -57,14 +57,13 @@ function runTests () {
 
 function buildArtifact () {
   # Invoking run-test.sh to install gems and run all checks
-  # runTests
-  curl -L https://raw.githubusercontent.com/Alfresco/packer-common/master/run-test.sh --no-sessionid | bash -s
+  runTests
 
   if [ -s Berksfile ]; then
-    echo "[run-release.sh] Building Chef artifact with Berkshelf"
+    echo "[deploy-cookbook.sh] Building Chef artifact with Berkshelf"
     rm -rf Berksfile.lock *.tar.gz; berks package berks-cookbooks.tar.gz
   elif [ -d data_bags ]; then
-    echo "[run-release.sh] Building Chef Databags artifact"
+    echo "[deploy-cookbook.sh] Building Chef Databags artifact"
     rm -rf *.tar.gz; tar cfvz alfresco-databags.tar.gz ./data_bags
   fi
 }
@@ -75,7 +74,7 @@ function getCurrentVersion () {
 }
 
 function deploy () {
-  echo "[run-release.sh] Deploy $1"
+  echo "[deploy-cookbook.sh] Deploy $1"
   repo_name=$MVN_REPO_ID
 
   mvn deploy:deploy-file -Dfile=$(echo *.tar.gz) -DrepositoryId=$MVN_REPO_CREDS_ID -Durl=$MVN_REPO_URL/content/repositories/$repo_name -DgroupId=$GROUP_ID  -DartifactId=$ARTIFACT_ID -Dversion=$1 -Dpackaging=tar.gz
