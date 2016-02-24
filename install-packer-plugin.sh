0f8c5895435eea695f5f135b3d87439d2f4c6452
Maurizio Pillitu <maoo@apache.org>
2016-02-24 11:24:19 +0000

    Delete install-packer-plugin.sh

--- install-packer-plugin.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -L --no-sessionid https://github.com/Alfresco/alfresco-spk/raw/vagrant-packer-plugin/pkg/vagrant-packer-plugin-0.5.0.gem > ~/.vagrant.d/vagrant-packer-plugin-0.5.0.gem
-
-vagrant plugin install ~/.vagrant.d/vagrant-packer-plugin-0.5.0.gem
