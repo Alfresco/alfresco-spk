64c89324579cb0145789402aebfceb79b13829a9
Enzo Rivello <vincenzo.rivello@gmail.com>
2016-03-01 13:03:49 +0000

    setting nils in the finalize

--- lib/vagrant-packer-plugin/config.rb
+++ lib/vagrant-packer-plugin/config.rb
@@ -18,10 +18,10 @@ module VagrantPlugins
 				@work_dir = "#{Dir.home}/.vagrant.d/data/packer-plugin" if @work_dir == UNSET_VALUE
 				@box_url = 'http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_centos-7.2_chef-provisionerless.box' if @box_url == UNSET_VALUE
 				@box_name = "opscode_centos-7.2" if @box_name == UNSET_VALUE
-				# @cookbooks_url = "./berks-cookbooks.tar.gz" if @cookbooks_url == UNSET_VALUE
-				# @databags_url = '' if @databags_url == UNSET_VALUE
+			  @cookbooks_url = nil if @cookbooks_url == UNSET_VALUE
+				@databags_url = nil if @databags_url == UNSET_VALUE
 				@ks_template = "" if @ks_template == UNSET_VALUE
-				# @berksfile = "Berksfile" if @berksfile == UNSET_VALUE
+				@berksfile = nil if @berksfile == UNSET_VALUE
 			end
 		end
 	end
