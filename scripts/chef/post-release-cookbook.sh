4737af9b367f94624e18d31ef2bdf77afae407fa
Maurizio Pillitu <maoo@apache.org>
2016-03-03 19:49:44 +0100

    Update post-release-cookbook.sh
    fix github_changelog_generator installation

--- scripts/chef/post-release-cookbook.sh
+++ scripts/chef/post-release-cookbook.sh
@@ -16,7 +16,7 @@ function incrementVersion () {
 
   echo "[post-release-cookbook.sh] Incrementing version from $currentVersion to $nextVersion"
 
-  if grep -L github_changelog_generator gems.list; then
+  if gem list | grep github_changelog_generator; then
     PKG_CONFIG_PATH=/opt/chefdk/embedded/lib/pkgconfig gem install nokogiri
     gem install github_changelog_generator
   fi
