32ec73f43bf8500b2d35bed3ddaf09abd54792f9
Nic Doye <nic.doye@alfresco.com>
2017-03-23 10:59:46 +0000

    grep version is wrong - as only looks for one quote style

--- scripts/chef/pre-release-cookbook.sh
+++ scripts/chef/pre-release-cookbook.sh
@@ -3,7 +3,7 @@
 # You need to export GIT_REPO=git@github.com:YourAccount/YourProject.git before calling this script
 
 function getCurrentVersion () {
-  version=`cat metadata.rb| grep version|awk '{print $2}' | tr -d \'`
+  version=$(grep version metadata.rb | awk '{print $2}' | tr -d \'\”)
   echo $version
 }
 
